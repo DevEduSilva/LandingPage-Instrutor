0ca31020d64825b90868ebda72fb19aebfd101df
Eduardo Silva <silvapereira1608gb@gmail.com>
2024-09-06 09:02:46 -0300

    Feat: Feito O JS do header

---
+++ assets/js/goToFromHeader.js
@@ -0,0 +1,35 @@
+function goTohome() {
+  const ctaabout = document.querySelector("#inicio");
+  window.scrollTo({
+    top: ctaabout.offsetTop - 100,
+    behavior: "smooth",
+  });
+}
+function goToAbout() {
+  const ctaabout = document.querySelector("#gotoabout");
+  window.scrollTo({
+    top: ctaabout.offsetTop - 100,
+    behavior: "smooth",
+  });
+}
+function goToDicas() {
+  const ctaabout = document.querySelector("#gotodicas");
+  window.scrollTo({
+    top: ctaabout.offsetTop - 100,
+    behavior: "smooth",
+  });
+}
+function goToSimulado() {
+  const ctaabout = document.querySelector("#gotosimulado");
+  window.scrollTo({
+    top: ctaabout.offsetTop - 100,
+    behavior: "smooth",
+  });
+}
+function goToContato() {
+  const ctaabout = document.querySelector("#gotocontato");
+  window.scrollTo({
+    top: ctaabout.offsetTop - 100,
+    behavior: "smooth",
+  });
+}
