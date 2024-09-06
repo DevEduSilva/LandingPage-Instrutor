0d2caf4fe6abbb0528c5938916c15a98b1c27106
Eduardo Silva <silvapereira1608gb@gmail.com>
2024-09-06 09:56:20 -0300

    feat: Validacao dos campos do usuarios

--- assets/js/validacaoContact.js
+++ assets/js/validacaoContact.js
@@ -36,3 +36,25 @@ document
       event.preventDefault(); // Impede o envio se o formulário não for válido
     }
   });
+
+// Função para transformar letras em maiúsculas
+function tornarMaiusculas(campo) {
+  document.getElementById(campo).addEventListener("input", function (letters) {
+    letters.target.value = letters.target.value.toUpperCase();
+  });
+}
+
+tornarMaiusculas("nome");
+tornarMaiusculas("mensagem");
+
+// Validando a formatação dos números de telefone
+document.getElementById("telefone").addEventListener("input", function (e) {
+  let telefone = e.target.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+  if (telefone.length > 0) {
+    telefone = telefone.replace(/^(\d{2})(\d)/g, "($1) $2"); // Adiciona os parênteses e o espaço
+  }
+  if (telefone.length >= 10) {
+    telefone = telefone.replace(/(\d{5})(\d)/, "$1-$2"); // Adiciona o hífen
+  }
+  e.target.value = telefone; // Atualiza o valor do campo de entrada
+});
