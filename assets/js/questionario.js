561cb8ae85dcf1381f498eaf129c0a8c05ac5f97
Eduardo Silva <silvapereira1608gb@gmail.com>
2024-10-17 23:41:28 -0300

    feat: pagina questionario feita

---
+++ assets/js/questionario.js
@@ -0,0 +1,150 @@
+import { questions } from './questoes.js'
+
+// Neste JS vou criar variaveis usadas para o DOM utlilizando o $ (Em outras palavras, informa-se que já existe essa var no html direto)
+const $startGameButton = document.querySelector(".start-quiz")
+const $questionsContainer = document.querySelector(".questions-container")
+const $answersContainer = document.querySelector(".answers-container")
+const $questionText = document.querySelector(".question")
+const $nextQuestionButton = document.querySelector(".next-question")
+const $statusGood = document.querySelector(".feedbackGood")
+const $statusBad = document.querySelector(".feedbackBad")
+const $messagemInicial = document.querySelector(".messagemInicial-container")
+
+// Indicador de questão atual
+let currentQuestionIndex = 0
+// variavel feita para contabilizar respostas corretas
+let totalCorrect = 0
+
+// Função de click para iniciar o quiz
+$startGameButton.addEventListener("click", startGame)
+// Função de click para avançar para a próxima questão
+$nextQuestionButton.addEventListener("click", displayNextQuestion)
+
+// Função de iniciar o quiz e mostar a parte de perguntas
+function startGame() {
+    $startGameButton.classList.add("hide")
+    $questionsContainer.classList.remove("hide")
+    $messagemInicial.classList.add("hide")
+
+    displayNextQuestion()
+}
+
+function displayNextQuestion() {
+    //resetar estado do quiz
+    resetState()
+
+    if (questions.length === currentQuestionIndex) {
+        // return dizendo para não continuar caso atendido
+        return finishGame();
+    }
+
+    // Verifica se ainda há questões e insere
+    $questionText.textContent = `${currentQuestionIndex + 1}) ${questions[currentQuestionIndex].question}`
+
+    // Mostrar as respostas disponíveis para a pergunta atual
+    questions[currentQuestionIndex].answers.forEach(answer => {
+        const newAnswer = document.createElement("button")
+        newAnswer.classList.add("answer", "button")
+        newAnswer.textContent = answer.text
+
+        // Gravar o estado da resposta para verificar se foi correta no futuro
+        if (answer.correct) {
+            newAnswer.dataset.correct = answer.correct
+        }
+
+        $answersContainer.appendChild(newAnswer)
+
+        //feito para capturar qual resposta foi selecionada
+        newAnswer.addEventListener("click", selectAnswer)
+    })
+}
+
+
+//resetar estado do quiz
+function resetState() {
+    // verifica se há um elemento filho na variavels seguinte
+    while ($answersContainer.firstChild) {
+        // remover elementos filhos
+        $answersContainer.removeChild($answersContainer.firstChild)
+    }
+
+    //feito para remover efeitos de retorno visual (incorreto ou correto)
+    document.body.removeAttribute("class")
+
+    // Deixará o botão de proxima oculta até selecionar outra resposta novamente
+    $statusGood.classList.add("hide")
+    $statusBad.classList.add("hide")
+    $nextQuestionButton.classList.add("hide")
+}
+
+// armezenar resposta do user
+function selectAnswer(event) {
+    const answerClicked = event.target
+
+    // Verificar se a resposta foi correta
+    if (answerClicked.dataset.correct) {
+        // altera o fundo para verde para confirmar a resposta
+        document.body.classList.add("correct")
+        $statusGood.classList.remove("hide")
+        // incrementar o total de respostas corretas
+        totalCorrect++
+    } else {
+        // altera o fundo para vermelho
+        document.body.classList.add("incorrect")
+        $statusBad.classList.remove("hide")
+
+    }
+
+    document.querySelectorAll(".answer").forEach(button => {
+        if (button.dataset.correct) {
+            // altera o botão para verde para confirmar a resposta
+            button.classList.add("correct")
+        } else {
+            // altera o botao para verde para confirmar a resposta
+            button.classList.add("incorrect")
+        }
+        // desabilita o botão
+        button.disabled = true
+    });
+
+    $nextQuestionButton.classList.remove("hide")
+    currentQuestionIndex++
+}
+
+// Função para terminar o quiz e mostrar as estatísticas
+function finishGame() {
+    const totalQuestion = questions.length
+    // Mostrar o resultado do quiz arredondado e sem ser em decimal
+    const performance = Math.floor(totalCorrect * 100 / totalQuestion)
+
+    // Mostrar as estatísticas do quiz
+    let message = ""
+
+    switch (true) {
+        case (performance >= 100):
+            message = "Parabéns, você todas as questões!"
+            break
+        case (performance >= 70):
+            message = `Aprovado! Parabéns, você acertou ${performance}% do simulado!!`
+            break
+        default:
+            message = "Não atingiu o requisito! Pode melhorar, entre em contato comigo e vamos trilhar junto esse sonho!"
+    }
+
+    $questionsContainer.innerHTML = `
+        <p class="final-message">
+            Você acertou <strong>${totalCorrect}</strong> de ${totalQuestion} questões!
+            <br>
+            <span>${message}</span>
+        </p>
+        <!--AQUI FORÇA A PÁGINA À REINICIAR-->
+        <div class='finalOrganizadoButtons'>
+            <button onclick=window.location.reload() class="button">
+                REFAZER SIMULADO
+            </button>
+            <button onclick=window.location.href='index.html' class="button">
+                PÁGINA INICIAL
+            </button>
+        </div>
+    `
+}
---
+++ assets/js/questoes.js
@@ -0,0 +1,273 @@
+// para mostrar em outro arquivo, deve exportar usando o export 
+export const questions = [
+    {
+        question: "Parar o veículo sobre a faixa de pedestres na mudança de sinal luminoso é infração de trânsito de natureza:",
+        answers: [
+            { text: "leve", correct: false },
+            { text: "média", correct: true },
+            { text: "grave", correct: false },
+            { text: "gravíssima", correct: false }
+        ]
+    },
+    {
+        question: "O dever de dar prioridade em suas ações à defesa da vida, nela incluída a preservação da saúde e do meio ambiente, incumbe:",
+        answers: [
+            { text: "ao Sistema Nacional de Trânsito", correct: false },
+            { text: "ao CONTRAN", correct: false },
+            { text: "ao DETRAN", correct: false },
+            { text: "aos órgãos e entidades de trânsito pertencentes ao Sistema Nacional de Trânsito", correct: true }
+        ]
+    },
+    {
+        question: "O órgão responsável pela regulamentação dos padrões da qualidade do ar é o:",
+        answers: [
+            { text: "CONAMA", correct: true },
+            { text: "PROCONVE", correct: false },
+            { text: "DETRAN", correct: false },
+            { text: "MINISTÉRIO DA SAÚDE", correct: false }
+        ]
+    },
+    {
+        question: "Entre as técnicas de direção defensiva estão agrupados cinco elementos básicos:",
+        answers: [
+            { text: "conhecimento, atenção, previsão, habilidade e ação", correct: true },
+            { text: "atenção, habilidade, experiência de 2 anos, segurança e prudência", correct: false },
+            { text: "habilidade, ação, prudência, conhecimentos técnicos e previsão", correct: false },
+            { text: "atenção, previsão, habilidade, conhecimentos mecânicos e prudência", correct: false }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "bifurcação em cruz", correct: false },
+            { text: "entroncamento", correct: false },
+            { text: "cruzamento de vias", correct: true },
+            { text: "interseção de vias", correct: false }
+        ]
+    },
+    {
+        question: "Quando estacionar o veículo sobre as pistas de rolamento das estradas, o condutor será punido com:",
+        answers: [
+            { text: "multa, apenas", correct: false },
+            { text: "remoção do veículo e multa", correct: false },
+            { text: "apreensão da Carteira Nacional de Habilitação e multa", correct: false },
+            { text: "cassação da Carteira Nacional de Habilitação", correct: true }
+        ]
+    },
+    {
+        question: "Aos veículos reprovados na inspeção de segurança e na de emissão de gases poluentes e ruídos será aplicada:",
+        answers: [
+            { text: "Multa", correct: false },
+            { text: "Apreensão do veículo", correct: false },
+            { text: "Recolhimento do veículo", correct: false },
+            { text: "Medida administrativa de retenção", correct: true }
+        ]
+    },
+    {
+        question: "O sistema que mantém a temperatura do motor sempre em condições de funcionamento chama-se:",
+        answers: [
+            { text: "Alimentação", correct: false },
+            { text: "Elétrico", correct: false },
+            { text: "Arrefecimento", correct: true },
+            { text: "Inflamação", correct: false }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "Ultrapassar os pedestres pela esquerda", correct: false },
+            { text: "Pedestre, ande pela esquerda", correct: true },
+            { text: "Pedestre, ultrapasse sempre pela esquerda", correct: false },
+            { text: "Atenção, pedestres circulando pela esquerda", correct: false }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "altura máxima permitida", correct: false },
+            { text: "largura mínima permitida", correct: false },
+            { text: "peso máximo permitido", correct: false },
+            { text: "altura limitada", correct: true }
+        ]
+    },
+    {
+        question: "São vias rurais:",
+        answers: [
+            { text: "vias de trânsito rápido", correct: false },
+            { text: "rodovias e estradas", correct: true },
+            { text: "vias locais e coletoras", correct: false },
+            { text: "estradas e vias preferenciais", correct: false }
+        ]
+    },
+    {
+        question: "Para habilitar-se na categoria 'D', o candidato deverá ser:",
+        answers: [
+            { text: "maior de 18 anos", correct: false },
+            { text: "penalmente imputável, não importando sua idade", correct: false },
+            { text: "menor de 21 anos", correct: false },
+            { text: "maior de 21 anos", correct: true }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "peso bruto total máximo proibido", correct: false },
+            { text: "peso bruto total mínimo permitido", correct: false },
+            { text: "peso bruto permitido", correct: false },
+            { text: "peso bruto total máximo permitido", correct: true }
+        ]
+    },
+    {
+        question: "É obrigatório o uso do cinto de segurança para condutor e passageiros em todas as vias do território nacional, salvo em situações regulamentadas:",
+        answers: [
+            { text: "pelo CONTRAN", correct: true },
+            { text: "pelo Sistema Nacional de Trânsito", correct: false },
+            { text: "pelo CETRAN", correct: false },
+            { text: "nenhuma das alternativas", correct: false }
+        ]
+    },
+    {
+        question: "Parar o veículo sobre a faixa de pedestres na mudança de sinal luminoso é infração de trânsito de natureza:",
+        answers: [
+            { text: "Leve", correct: false },
+            { text: "Média", correct: true },
+            { text: "Grave", correct: false },
+            { text: "Gravíssima", correct: false }
+        ]
+    },
+    {
+        question: "Em dias de chuva, com a pista escorregadia, a distância de segurança entre dois veículos deverá ser de:",
+        answers: [
+            { text: "4 segundos", correct: true },
+            { text: "2 segundos", correct: false },
+            { text: "6 segundos", correct: false },
+            { text: "10 segundos", correct: false }
+        ]
+    },
+    {
+        question: "O julgamento dos recursos interpostos pelos infratores compete:",
+        answers: [
+            { text: "ao CONTRAN", correct: false },
+            { text: "ao CETRAN", correct: false },
+            { text: "ao CONTRANDIFE", correct: false },
+            { text: "à JARI", correct: true }
+        ]
+    },
+    {
+        question: "A diasina é um gás expelido pelos motores movidos a:",
+        answers: [
+            { text: "Eletricidade", correct: false },
+            { text: "Gasolina", correct: false },
+            { text: "Álcool", correct: false },
+            { text: "Diesel", correct: true }
+        ]
+    },
+    {
+        question: "Tempo de reação é aquele que transcorre entre:",
+        answers: [
+            { text: "o pisar no freio e a imobilização total do veículo", correct: false },
+            { text: "o acionar a embreagem e o freio", correct: false },
+            { text: "a visualização do perigo e o pisar no freio", correct: true },
+            { text: "a percepção do perigo e a parada", correct: false }
+        ]
+    },
+    {
+        question: "Se o candidato à habilitação for reprovado no exame de direção veicular, ele só poderá realizar novo exame após:",
+        answers: [
+            { text: "a qualquer tempo, não é necessário esperar nenhum prazo", correct: true },
+            { text: "14 dias da divulgação do resultado", correct: false },
+            { text: "15 dias da divulgação do resultado", correct: false },
+            { text: "8 dias da divulgação do resultado", correct: false }
+        ]
+    },
+    {
+        question: "De acordo com o art. 13 do Código de Trânsito Brasileiro, é órgão técnico vinculado ao CONTRAN:",
+        answers: [
+            { text: "o DETRAN", correct: false },
+            { text: "as Câmaras Temáticas", correct: true },
+            { text: "o CETRAN", correct: false },
+            { text: "a JARI", correct: false }
+        ]
+    },
+    {
+        question: "Afastar os curiosos, desligar o motor do veículo envolvido e orientar para que não fumem no local é uma ação do socorrista no caso de:",
+        answers: [
+            { text: "atropelamento", correct: false },
+            { text: "derramamento de combustível", correct: true },
+            { text: "obstáculo na via", correct: false },
+            { text: "acidente sem vítimas", correct: false }
+        ]
+    },
+    {
+        question: "Quando houver a certeza de que o veículo não apresentará problemas durante a operação, o condutor deverá:",
+        answers: [
+            { text: "desprezar as normas de segurança", correct: false },
+            { text: "manter a calma e agir com prudência", correct: true },
+            { text: "não observar o fluxo de veículos", correct: false },
+            { text: "acelerar a velocidade do veículo", correct: false }
+        ]
+    },
+    {
+        question: "No caso de acidente em que houve apenas danos materiais, a ocorrência deve ser registrada:",
+        answers: [
+            { text: "na delegacia de polícia", correct: true },
+            { text: "pelo Detran", correct: false },
+            { text: "na concessionária da rodovia", correct: false },
+            { text: "no Corpo de Bombeiros", correct: false }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "proibido estacionar", correct: true },
+            { text: "proibido parar", correct: false },
+            { text: "estacionamento regulamentado", correct: false },
+            { text: "estacionamento permitido", correct: false }
+        ]
+    },
+    {
+        question: "Quando um motorista for punido com a suspensão do direito de dirigir, a penalidade será de:",
+        answers: [
+            { text: "2 meses", correct: false },
+            { text: "12 meses", correct: false },
+            { text: "de 1 a 12 meses", correct: true },
+            { text: "3 meses", correct: false }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "Atenção, ciclistas na pista", correct: true },
+            { text: "Atenção, ciclistas devem andar pela pista", correct: false },
+            { text: "Atenção, ciclistas não podem andar pela pista", correct: false },
+            { text: "Atenção, ciclistas na calçada", correct: false }
+        ]
+    },
+    {
+        question: "A placa ao lado significa:",
+        answers: [
+            { text: "Proibido estacionar", correct: true },
+            { text: "Proibido parar", correct: false },
+            { text: "Atenção, cuidado com o estacionamento", correct: false },
+            { text: "Estacionamento permitido", correct: false }
+        ]
+    },
+    {
+        question: "O que o motorista deve fazer quando vê um sinal de Pare?",
+        answers: [
+            { text: "Parar, verificar se não há veículos ou pedestres e seguir", correct: true },
+            { text: "Reduzir a velocidade e seguir", correct: false },
+            { text: "Acelerar e passar", correct: false },
+            { text: "Parar somente se houver pedestres", correct: false }
+        ]
+    },
+    {
+        question: "Condições adversas para conduzir um veículo podem ser:",
+        answers: [
+            { text: "chuva, neblina e sinalização confusa", correct: true },
+            { text: "vários veículos na via", correct: false },
+            { text: "sol forte", correct: false },
+            { text: "trechos conhecidos", correct: false }
+        ]
+    }
+];
